71ba001b11ce8dcf4a9b285c469c8c82413c18da
Sreejith <L00163270@student.lyit.ie>
2021-10-08 15:33:08 +0530

    First commit

---
+++ pkg/__init__.py
@@ -0,0 +1,32 @@
+# --------------------
+# File      : .py
+# Created   : 08-10-2021 {TIME}
+# Author    : Sreejith
+# Version   : v1.0.0.
+# Licensing : (C) 2021  Sreejith , LYIT
+#             Available under  GNU Public License (GPL)
+# Description :
+# --------------------
+
+if __name__ == '__main_':
+    '''
+
+   Main method of application 
+
+
+
+   Linear programming only presented here wrt demo of lists
+
+
+
+   Parameters:
+
+    none
+
+
+
+   Returns:
+
+    none
+
+  '''
---
+++ pkg/tuple.py
@@ -0,0 +1,50 @@
+# --------------------
+# File      : .py
+# Created   : 08-10-2021 {TIME}
+# Author    : Sreejith
+# Version   : v1.0.0.
+# Licensing : (C) 2021  Sreejith , LYIT
+#             Available under  GNU Public License (GPL)
+# Description :
+# --------------------
+
+if __name__ == '__main_':
+    '''
+
+   Main method of application 
+
+   Linear programming only presented here wrt demo of lists
+
+   Parameters:
+
+    none
+
+   Returns:
+
+    none
+
+  '''
+
+empty_list = []
+
+student_id = ["L12345", "L54321" ]
+student_subject = ["Java_ooprogramming" , "Python_Scripting"]
+grade_student1 ={"L12345": 40}
+grade_student2 = {"L12345":69}
+
+
+
+    Student1_details = ["L12345","Java_ooprogramming","Python_Scripting"]
+    Student2_details = ["L54321", "Java_ooprogramming", "Python_Scripting"]
+
+    print("The details of Student 1 is ")
+    print(Student1_details)
+    print("The details of Student 2 is ")
+    print(Student2_details)
+
+
+
+print("Thank you for using my application")
+
+
+
